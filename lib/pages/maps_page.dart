dbdea48f15168d3af92153f882d99b5d0359305d
Markos <markosmora@live.com>
2023-11-26 04:56:39 -0500

    Deleted repeated import on maps_page

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/login_page.dart';
 import 'package:frontend/pages/profile_page.dart';
-import 'package:frontend/pages/profile_page.dart';
 import 'package:frontend/pages/report_bike_dialog.dart';
 import 'package:frontend/reqs.dart';
 import 'package:geolocator/geolocator.dart';
